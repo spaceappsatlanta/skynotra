1679c07b067465218e3bd0095c3b655604a3d7ee
Zac Stewart <zgstewart@gmail.com>
2013-04-20 22:53:10 -0400

    Add images endpoint

--- skynotra.rb
+++ skynotra.rb
@@ -1,5 +1,7 @@
 require 'bundler'
 require 'json'
+require 'net/http'
+require 'uri'
 
 Bundler.require if defined?(Bundler)
 
@@ -16,10 +18,28 @@ module Skynotra
       deliver(observations)
     end
 
+    get '/images/:key.json' do
+      image_url = get_image_url(params[:key])
+      deliver({image_url: image_url})
+    end
+
+    get '/images/:key.gif' do
+      content_type :gif
+      image_url = URI.parse(get_image_url(params[:key]))
+      Net::HTTP.get_response(image_url).body
+    end
+
     private
     def deliver(object)
       content_type :json
       JSON.dump object
+    end
+
+    def get_image_url(key)
+      image_request = SkyMorph::ImageRequest.new(params[:key])
+      image_response = image_request.fetch
+      image_parser = SkyMorph::ImageParser.new
+      image_parser.parse_html(image_response)
     end
   end
 end
