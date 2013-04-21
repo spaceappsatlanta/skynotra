76fe360ffc9fa3af3cca3884cc8432176305d5e2
Zac Stewart <zgstewart@gmail.com>
2013-04-20 20:38:54 -0400

    Add endpoint for observations

--- skynotra.rb
+++ skynotra.rb
@@ -1,4 +1,5 @@
 require 'bundler'
+require 'json'
 
 Bundler.require if defined?(Bundler)
 
@@ -6,6 +7,19 @@ module Skynotra
   class Application < Sinatra::Application
     get '/' do
       'Hello world'
+    end
+
+    get '/observations/:target' do
+      target_request = SkyMorph::TargetRequest.new(params[:target])
+      target_response = target_request.fetch
+      observations = SkyMorph::ObservationTableParser.parse(target_response)
+      deliver(observations)
+    end
+
+    private
+    def deliver(object)
+      content_type :json
+      JSON.dump object
     end
   end
 end
