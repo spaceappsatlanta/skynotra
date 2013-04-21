e2a0fa1192b5067ab8f3a37db65f2cc37e9ed510
James Dabbs <jamesdabbs@gmail.com>
2013-04-21 12:05:34 -0400

    Add simple Redis caching

--- Gemfile
+++ Gemfile
@@ -1,4 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'pry'
+gem 'redis'
 gem 'sinatra'
 gem 'skymorph', git: 'git://github.com/spaceappsatlanta/skymorph.git'
--- skynotra.rb
+++ skynotra.rb
@@ -1,9 +1,24 @@
 require 'bundler'
 require 'json'
 require 'net/http'
+require 'redis'
 require 'uri'
 
 Bundler.require if defined?(Bundler)
+
+before do
+  $redis = Redis.new
+end
+
+def cache key, &block
+  if val = $redis.get(key)
+    val
+  else
+    val = block.call
+    $redis.set key, val
+    val
+  end
+end
 
 module Skynotra
   class Application < Sinatra::Application
@@ -12,13 +27,19 @@ module Skynotra
     end
 
     get '/observations/:target' do
-      observations = SkyMorph::Observation.find(params[:target]).map(&:to_hash)
-      deliver(observations)
+      content_type :json
+      cache request.path do
+        observations = SkyMorph::Observation.find(params[:target]).map(&:to_hash)
+        JSON.dump observations
+      end
     end
 
     get '/images/:key.json' do
-      image_url = get_image_url(params[:key])
-      deliver({image_url: image_url})
+      content_type :json
+      cache request.path do
+        image_url = get_image_url(params[:key])
+        JSON.dump { image_url: image_url }
+      end
     end
 
     get '/images/:key.gif' do
@@ -28,10 +49,6 @@ module Skynotra
     end
 
     private
-    def deliver(object)
-      content_type :json
-      JSON.dump object
-    end
 
     def get_image_url(key)
       image_request = SkyMorph::ImageRequest.new(params[:key])
